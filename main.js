eef959a95e62ba02676653fea8814397a27c8500
Andrew Maurice <andromulin@gmail.com>
2018-07-01 10:07:27 -0500

    javascript and html work (no style yet)

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 //the inputted name is valid using Regexp and returning the 
 //appropriate result
 
+//name vaidation function
+function validateName () {
+    //retrieve inputted name from html nameInput input box       
+    var name = document.getElementById("nameInput").value;
+
+    //test that no characters other than letters, ', and space are present in the name,
+    //where the text is only updated for a valid name
+    if (!/[^a-z\'\ ]/gi.test(name)){
+        $("#nameDisplay").text("Hello " + name + "!");
+    }
+}
